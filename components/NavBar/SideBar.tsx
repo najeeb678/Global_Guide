8fcf1e4cdf0247352b1764037e306b172e9e5166
najeeb678 <najibqazi777@gmail.com>
2024-07-07 23:13:26 +0500

    adding icons to sidebar

--- components/NavBar/SideBar.tsx
+++ components/NavBar/SideBar.tsx
@@ -1,4 +1,5 @@
-import * as React from "react";
+
+import React from "react";
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
 import Button from "@mui/material/Button";
@@ -8,50 +9,66 @@ import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
 import styles from "./navbar.module.css";
 import MenuIcon from "@mui/icons-material/Menu";
 import Image from "next/image";
 import { Typography } from "@mui/material";
+
 interface SideBarProps {
   toggleSidebar: () => void;
 }
+
 export default function SideBar({ toggleSidebar }: SideBarProps) {
   const [open, setOpen] = React.useState(false);
 
   const toggleDrawer = (newOpen: boolean) => () => {
     setOpen(newOpen);
-    toggleSidebar(); 
+    toggleSidebar();
   };
 
-   const sidebarLinks = [
+  const sidebarLinks = [
     {
-      imgURL: "/icons/home.svg",
+      imgURL: "/images/home.svg",
       route: "/",
-      label: "Home",
+      label: "Brands",
     },
     {
-      imgURL: "/icons/dollar-circle.svg",
-      route: "/my-banks",
-      label: "My Banks",
+      imgURL: "/images/dollar-circle.svg",
+      route: "/",
+      label: "Releases",
     },
     {
-      imgURL: "/icons/transaction.svg",
-      route: "/transaction-history",
-      label: "Transaction History",
+      imgURL: "/images/transaction.svg",
+      route: "/",
+      label: "Distilleries",
     },
     {
-      imgURL: "/icons/money-send.svg",
-      route: "/payment-transfer",
-      label: "Transfer Funds",
+      imgURL: "/images/money-send.svg",
+      route: "/",
+      label: "Companies",
+    },
+    {
+      imgURL: "/images/transaction.svg",
+      route: "/",
+      label: "All mail",
+    },
+    {
+      imgURL: "/images/money-send.svg",
+      route: "/",
+      label: "Sign In",
+    },
+    {
+      imgURL: "/images/connect-bank.svg",
+      route: "/",
+      label: "Contact",
     },
   ];
+
   const DrawerList = (
     <Box
       sx={{ width: 250, backgroundColor: "#14171c", color: "#FFFFFF" }}
       role="presentation"
-      onClick={toggleDrawer(false)} 
+      onClick={toggleDrawer(false)}
     >
       <Box className={styles.logoContainer} mt={3} mb={3}>
         <Image src="/images/Logo.svg" width={22} height={22} alt="logo" />
@@ -60,28 +77,19 @@ export default function SideBar({ toggleSidebar }: SideBarProps) {
         </Typography>
       </Box>
       <List>
-        {["Brands", "Releases", "Distilleries", "Companies"].map(
-          (text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon sx={{ color: "#FFFFFF" }}>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} sx={{ color: "#FFFFFF" }} />
-              </ListItemButton>
-            </ListItem>
-          )
-        )}
-      </List>
-      <Divider />
-      <List>
-        {["All mail", "Sign In", "Contact"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
+        {sidebarLinks.map((link) => (
+          <ListItem key={link.label} disablePadding>
+             <ListItemButton
+              sx={{
+                '&:hover': {
+                  backgroundColor: '#BA9775',
+                },
+              }}
+            >
               <ListItemIcon sx={{ color: "#FFFFFF" }}>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                <Image src={link.imgURL} width={24} height={24} alt={link.label} />
               </ListItemIcon>
-              <ListItemText primary={text} sx={{ color: "#FFFFFF" }} />
+              <ListItemText primary={link.label} sx={{ color: "#FFFFFF" }} />
             </ListItemButton>
           </ListItem>
         ))}
@@ -111,3 +119,4 @@ export default function SideBar({ toggleSidebar }: SideBarProps) {
     </Box>
   );
 }
+
